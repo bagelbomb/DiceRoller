8ea7f1c6e611e4aaac924ec36c971c53c2d1ceaa
Tanner Iverson <54693798+bagelbomb@users.noreply.github.com>
2019-09-13 15:15:47 -0700

    in MainActivity, "created" the roll button, set it's text, and set an OnClickListener to call method rollDice(); in rollDice(), "created" the resultText TextView, generated random int between 1 and 6, and set text to the result

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.roll_button);
+
+        rollButton.setText("Let's Roll");
+
+        // Lambda only works with compatibility set to Java 8, hope that doesn't cause problems
+        rollButton.setOnClickListener(e -> {
+            rollDice();
+        });
+    }
+
+    public void rollDice() {
+
+        TextView resultText = findViewById(R.id.result_text);
+        int randomInt = new Random().nextInt(6) + 1;
+
+        resultText.setText(Integer.toString(randomInt));
+
     }
 }

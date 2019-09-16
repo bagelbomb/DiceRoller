a840284ff0c8fafc2e8684172cf29baeef4d8e01
Tanner Iverson <54693798+bagelbomb@users.noreply.github.com>
2019-09-16 15:24:20 -0700

    replaced TextView with ImageView, button press displays appropriate dice image instead of text

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,10 +30,35 @@ public class MainActivity extends AppCompatActivity {
 
     public void rollDice() {
 
-        TextView resultText = findViewById(R.id.result_text);
+        //TextView resultText = findViewById(R.id.result_text);
         int randomInt = new Random().nextInt(6) + 1;
 
-        resultText.setText(Integer.toString(randomInt));
+        //resultText.setText(Integer.toString(randomInt));
+
+        int drawableResource;
+
+        switch (randomInt) {
+            case 1:
+                drawableResource = R.drawable.dice_1;
+                break;
+            case 2:
+                drawableResource = R.drawable.dice_2;
+                break;
+            case 3:
+                drawableResource = R.drawable.dice_3;
+                break;
+            case 4:
+                drawableResource = R.drawable.dice_4;
+                break;
+            case 5:
+                drawableResource = R.drawable.dice_5;
+                break;
+            default:
+                drawableResource = R.drawable.dice_6;
+        }
+
+        ImageView diceImage = findViewById(R.id.dice_image);
+        diceImage.setImageResource(drawableResource);
 
     }
 }
